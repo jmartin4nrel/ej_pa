3a65832326db2d6d886ffd72471782886e115159
jmartin4 <jonathan.martin@nrel.gov>
2024-12-11 17:03:37 -0700

    Running without iron

--- example_plant/run_example_plant.py
+++ example_plant/run_example_plant.py
@@ -27,13 +27,13 @@ import openmdao.api as om
 from utilities.load_library_inputs import load_ore_cost, load_tech_capex
 from utilities.download_site_resources import download_site_resources
 
-def run_example_plant(run_pre_iron=True, ore=None, tech=None, location=[45.0, -90.0]):
+def run_example_plant(run_pre_profast=True, ore=None, tech=None, location=[45.0, -90.0]):
     # Decide if running a new simulation or loading a previously run simulation
     run_new = True
     filename = "example_plant"
 
     # Decide if running all of the parts of GreenHEART before the green iron model and saving output
-    save_pre_iron = True # Output pre-green iron must be saved once before save_pre_iron can be set to False
+    save_pre_profast = True # Output pre-green iron must be saved once before save_pre_profast can be set to False
 
     # Decide if running OpenMDAO problem or straight GreenHEART simulation (True for OpenMDAO)
     run_om = False
@@ -70,33 +70,39 @@ def run_example_plant(run_pre_iron=True, ore=None, tech=None, location=[45.0, -9
             save_plots=False,
             use_profast=True,
             post_processing=False,
-            run_pre_iron=run_pre_iron,
-            save_pre_iron=save_pre_iron,
-            pre_iron_fn=output_filepath + filename + "_pre_iron",
+            run_pre_profast=run_pre_profast,
+            save_pre_profast=save_pre_profast,
+            pre_profast_fn=output_filepath + filename + "_pre_profast",
             iron_modular=False,
             incentive_option=1,
             plant_design_scenario=9,
             output_level=7,
         )
 
-        # Modify ore, tech, location
-        if ore != None:
-            config.greenheart_config["iron"]["ore_type"] = ore
-        if tech != None:
-            config.greenheart_config["iron"]["technology"] = tech
-        if config.greenheart_config['iron']['site']:
+        # # Modify ore, tech, location
+        # if ore != None:
+        #     config.greenheart_config["iron"]["ore_type"] = ore
+        # if tech != None:
+        #     config.greenheart_config["iron"]["technology"] = tech
+        # if config.greenheart_config['iron']['site']:
+        
+        # Modify location
+        if config.greenheart_config['site']['resource_dir']:
             lat = location[0]
             lon = location[1]
             config.hopp_config["site"]["data"]["lat"] = lat 
             config.hopp_config["site"]["data"]["lon"] = lon 
-            config.greenheart_config['iron']['site']['resource_dir'] =  str(Path(os.path.abspath(__file__)).parent) + \
-            config.greenheart_config['iron']['site']['resource_dir']
-            fp = config.greenheart_config['iron']['site']['resource_dir']
+            # config.greenheart_config['iron']['site']['resource_dir'] =  str(Path(os.path.abspath(__file__)).parent) + \
+            # config.greenheart_config['iron']['site']['resource_dir']
+            # fp = config.greenheart_config['iron']['site']['resource_dir']
+            config.greenheart_config['site']['resource_dir'] = str(Path(os.path.abspath(__file__)).parent) + \
+                config.greenheart_config['site']['resource_dir']
+            fp = config.greenheart_config['site']['resource_dir']
             download_site_resources(config,fp)     
         
-        # Load ore- and tech- specific costs
-        config = load_ore_cost(config, ore_cost_filepath)
-        config = load_tech_capex(config, tech_capex_filepath)
+        # # Load ore- and tech- specific costs
+        # config = load_ore_cost(config, ore_cost_filepath)
+        # config = load_tech_capex(config, tech_capex_filepath)
 
     # Run/load GreenHEART simulation
     if run_om:
@@ -114,21 +120,21 @@ def run_example_plant(run_pre_iron=True, ore=None, tech=None, location=[45.0, -9
 
         lcoe = prob.get_val("lcoe", units="USD/(MW*h)")
         lcoh = prob.get_val("lcoh", units="USD/kg")
-        lcoi = prob.get_val("lcoi", units="USD/t")
+        # lcoi = prob.get_val("lcoi", units="USD/t")
 
     else:
         # Run not using OpenMDAO
         if run_new:
             lcoe, lcoh, iron_finance, ammonia_finance = run_greenheart(config)
-            lcoi = iron_finance.sol["price"]
+            # lcoi = iron_finance.sol["price"]
 
             # Save GreenHEART data (lcoe, lcoh, and IronCostModelOutputs)
             output = open(output_filepath + filename + "_lcoe.txt", "w")
             output.write(str(lcoe))
             output = open(output_filepath + filename + "_lcoh.txt", "w")
             output.write(str(lcoh))
-            output = open(output_filepath + filename + "_if.pkl", "wb")
-            pickle.dump(iron_finance, output)
+            # output = open(output_filepath + filename + "_if.pkl", "wb")
+            # pickle.dump(iron_finance, output)
 
         # Load from saved files
         else:
@@ -136,13 +142,13 @@ def run_example_plant(run_pre_iron=True, ore=None, tech=None, location=[45.0, -9
             lcoe = float(input.read())
             input = open(output_filepath + filename + "_lcoh.txt")
             lcoh = float(input.read())
-            input = open(output_filepath + filename + "_if.pkl", "rb")
-            iron_finance = pickle.load(input)
-            lcoi = iron_finance.sol["price"]
+            # input = open(output_filepath + filename + "_if.pkl", "rb")
+            # iron_finance = pickle.load(input)
+            # lcoi = iron_finance.sol["price"]
 
     # print("LCOE: ", lcoe, "[$/MWh]")
-    # print("LCOH: ", lcoh, "[$/kg]")
-    print("LCOI: ", lcoi, "[$/metric-tonne]")
+    print("LCOH: ", lcoh, "[$/kg]")
+    # print("LCOI: ", lcoi, "[$/metric-tonne]")
 
 
 # Run as script
